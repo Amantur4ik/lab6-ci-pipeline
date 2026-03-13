be64dda6afbdf60c3cb9a6668734c44b68a623f8
Amantur4ik <amanturosmanbaev@gmail.com>
2026-03-13 16:20:10 +0600

    Обновил код и workflow

--- src/calculator.py
+++ src/calculator.py
@@ -1,5 +1,5 @@
 """Simple calculator module."""
-x = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
 
 def add(a: float, b: float) -> float:
     return a + b
@@ -17,7 +17,3 @@ def divide(a: float, b: float) -> float:
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
-
-
-def test_add():
-    assert add(2, 3) == 6
