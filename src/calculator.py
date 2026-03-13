bd18019bb7aee75085cdae18c03d303a05d6b8b7
Amantur4ik <amanturosmanbaev@gmail.com>
2026-03-13 15:44:09 +0600

    Break test to show CI failure

--- src/calculator.py
+++ src/calculator.py
@@ -3,6 +3,8 @@
 
 def add(a: float, b: float) -> float:
     return a + b
+
+
 def subtract(a: float, b: float) -> float:
     return a - b
 
@@ -15,3 +17,7 @@ def divide(a: float, b: float) -> float:
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
+
+def test_add():
+    assert add(2, 3) == 6
